071ed6a2831841cc21fdf813a509df2ffb5ebc11
jLuseno161 <joyluseno2@gmail.com>
2021-06-01 12:51:09 +0300

    full code

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+# from .config import DevConfig
+from .config import config_options 
+from flask_bootstrap import Bootstrap
+
+
+# Initializing flask application
+app = Flask(__name__,instance_relative_config = True)
+
+# Setting up configuration
+app.config.from_object(config_options['development'])
+# app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+from app import views
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
---
+++ app/config.py
@@ -0,0 +1,35 @@
+# import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+# Enables Debug mode in the application
+    DEBUG = True
+
+#called to access 2 subclasses
+config_options = {
+    'development': DevConfig, 'production': ProdConfig
+}
